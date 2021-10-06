0e1720476b935de2c79beff8d1d85b5a8786afdc
Diego Polaco <dieghard@gmail.com>
2021-10-06 20:14:19 -0300

    acomodando Panel de Control

--- admin/vista/index.php
+++ admin/vista/index.php
@@ -163,7 +163,10 @@ if (isset($_GET['controlador'])) {
             <!-- END: dashhead -->
           </header>
           <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-          <script src="../../assets/js/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
           <!-- END: .main-heading -->
 
@@ -205,14 +208,8 @@ if (isset($_GET['controlador'])) {
       <!-- END: .app-container -->
 
       <?php include 'wireframe/footer.php'; ?>
-      <!--------------------------------------------------------------------------------- -->
-
 
       <!-- Bootstrap -->
-
-      <script src="../../assets/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-      <script src="../../assets/js/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-      <link href="../../assets/css/bootstrap.min.css" rel="stylesheet" />
 
       <!-- FIN Bootstrap -->
       <!-- SELECT2 -->
