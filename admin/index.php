aa382760b115724cdf097d04545bf24ebea5376c
Diego Polaco <dieghard@gmail.com>
2021-10-08 11:52:37 -0300

    mejora Loggin

--- admin/index.php
+++ admin/index.php
@@ -6,50 +6,51 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Administción - POLAKOMMERCE</title>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+  <title>Administración - POLAKOMMERCE</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
   <link rel="stylesheet" href="../assets/vendor/font-awesome/css/font-awesome.css">
   <link rel="stylesheet" href="../assets/css/animate.css">
-  <link rel="stylesheet" href="../assets/css/loguin.css">
+  <!--<link rel="stylesheet" href="../assets/css/loguin.css">-->
+  <link rel="stylesheet" href="../assets/css/loguin2.css">
 
 </head>
 
 <body>
   <div class="container">
-
-    <!---div class="app"> -->
     <div class="row">
-      <!---<div class="text-center box shadow-5 animated fadeInLeft b-r-4 p-a-20">-->
-      <div class="wrapper fadeInDown">
-        <div id="formContent">
-          <!-- Icon -->
-          <div class="fadeIn first">
-            <!--<img src="../assets/img/biblioteca.jpg"  class="rounded mx-auto d-block img-fluid m-2" width="10%" height="10%"  id="icon"  alt="User Icon" />-->
-          </div>
-          <hr>
-          <div class="text-center box shadow-5 animated fadeInLeft b-r-4 p-a-20">ADMIN - POLAKOMMERCE</div>
-          <div class="form-group has-feedback m-1">
-            <input type="email" class="form-control fadeIn second" id="inputEmail" placeholder="Ingrese su Email">
-          </div>
-          <div class="form-group has-feedback m-1">
-            <input type="password" class="form-control fadeIn third" id="inputPassword" placeholder="Ingrese su Password">
-
-          </div>
-          <div class="form-group has-feedback m-1">
-            <div id="divUsuario"></div>
-            <button id="btn-login" class="btn btn-primary btn-lg m-3 ">Ingresar</button>
-            <div class="row m-3 text-center  justify-content-center align-items-center minh-100">
-              <a href="app-forgot.html">
-                <small>¿Olvido el password?</small>
-              </a>
+      <div class="col-md-6 mx-auto p-0">
+        <div class="card">
+          <h1 class="text-center">POLAKOMMERCE</h1>
+          <div class="login-box">
+            <div class="login-snip"> <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Ingreso</label> <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">¡Registrame!</label>
+              <div class="login-space">
+                <div class="login">
+                  <div class="group"> <label for="user" class="label">Mail</label> <input type="text" class="input" id="inputEmail" placeholder="Ingrese su Email"> </div>
+                  <div class="group"> <label for="pass" class="label">Password</label> <input type="password" class="input" data-type="password" id="inputPassword" placeholder="Ingrese su Password"> </div>
+                  <div class="group"> <input id="check" type="checkbox" class="check" checked> <label for="check"><span class="icon"></span> Mantenerme Logueado</label> </div>
+                  <div class="group"><button id="btn-login" class="btn btn-primary btn-lg m-3 ">Ingresar</button></div>
+                  <div class="hr"></div>
+                  <div class="foot"> <a href="#">¿Olvidaste tu Password?</a> </div>
+                </div>
+                <div class="sign-up-form">
+                  <div class="group"> <label for="user" class="label">Mail</label> <input id="user" type="text" class="input" placeholder="Ingrese su mail"> </div>
+                  <div class="group"> <label for="pass" class="label">Password</label> <input id="pass" type="password" class="input" data-type="password" placeholder="Crear password"> </div>
+                  <div class="group"> <label for="pass" class="label">Repetir Password</label> <input id="pass" type="password" class="input" data-type="password" placeholder="Repetir tu password"> </div>
+                  <div class="group"> <input type="submit" class="button" value="Registrate"> </div>
+                  <div class="hr"></div>
+                  <div class="foot"> <label for="tab-1">¿Ya estas registrado?</label> </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-      <script src="../assets/js/login.js"></script>
+    </div>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="../assets/js/login.js"></script>
 </body>
 
 </html>
